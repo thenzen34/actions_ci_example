05f6f13ec0c4c9a573554fa432b1b7c00bd2bde4
murashev <murashev@cetacs.com>
2021-08-31 10:10:19 +0300

    fix

--- src/app.py
+++ src/app.py
@@ -17,7 +17,7 @@ def validate_post_data(data: dict) -> bool:
 
 @app.route('/', methods=['GET'])
 def hello():
-    return 'Hello World1!'
+    return 'Hello World!'
 
 
 @app.route('/api', methods=['GET', 'POST'])
